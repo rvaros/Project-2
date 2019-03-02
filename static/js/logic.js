0e675ee654df204c013aed0828e213cf3ce0545c
rvaros <richvaros@gmail.com>
2019-03-02 14:02:12 -0500

    Many changes

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,42 @@
+var foreclosureJSON = '../../CurrentRecessionForeclosure2.json';
+
+var foreclosureMarkers = L.layerGroup();
+
+d3.json(foreclosureJSON, function(err,data) {
+    if(err) console.log("error fectching data");
+    var features = data.features;
+    console.log(features);
+    features.forEach((foreclosure,i) => {
+        var format = {
+            color: "#4169E1",
+            fillColor: "#4169E1",
+            fillOpacity: "3",
+            weight: "1",
+            radius: foreclosure.current * 11000
+        };
+        foreclosureMarkers.addLayer(L.circle([foreclosure.latitude,foreclosure.longitude], format)
+        );
+    });
+});
+
+d3.json(foreclosureJSON, function(err,data) {
+    if(err) console.log("error fectching data");
+    var features = data.features;
+    console.log(features);
+    features.forEach((foreclosure,i) => {
+        var format = {
+            color: "#B22222",
+            fillColor: "#B22222",
+            fillOpacity: "0.5",
+            weight: "1",
+            radius: foreclosure.recessionavg * 10000
+        };
+        foreclosureMarkers.addLayer(L.circle([foreclosure.latitude,foreclosure.longitude], format)
+            .bindPopup("<h3>" + foreclosure.state + "<h6><h6>Recession Average: " + foreclosure.recessionavg + "%" +"<h6><h6>Current: " + foreclosure.current + "%")
+        );
+    });
+});
+
 var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -27,10 +66,29 @@ var baseMaps = {
 var myMap = L.map("map", {
     center: [37, -100],
     zoom: 4,
-    layers: [OpenMapSurfer_Roads]
+    layers: [OpenMapSurfer_Roads, foreclosureMarkers]
 });
+
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = ["Recession Average", "Current"];
+    var colors = ["#B22222", "#4169E1"];
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h4>Foreclosure Percentage</h1>"
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\">"+limit+"</li>");
+    });
+
+    div.innerHTML += "<ul class=\"list-unstyled\">" + labels.join("") + "</ul>";
+    return div;
+};
 
 // Adding legend to the map
 legend.addTo(myMap);
 
-L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+L.control.layers(baseMaps).addTo(myMap);
