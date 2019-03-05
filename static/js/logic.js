4a44cd2a71e8144eab7929db2d0ad49f7c3d5a03
rvaros <richvaros@gmail.com>
2019-03-04 19:32:46 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -65,7 +65,7 @@ var myMap = L.map("map", {
 var legend = L.control({ position: "bottomright" });
 legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
-    var limits = ["Recession Average (Jan 2008 - Jun 2009)", "Current"];
+    var limits = ["Recession Average (Jan 2008 - Jun 2009)", "Current (September 2018)"];
     var colors = ["#B22222", "#4169E1"];
     var labels = [];
 
