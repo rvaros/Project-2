03f32d4991cb8b0b53df513ba956ae57f32a8014
rvaros <richvaros@gmail.com>
2019-03-04 19:20:10 -0500

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -41,11 +41,6 @@ var OpenMapSurfer_Roads = L.tileLayer('https://maps.heigit.org/openmapsurfer/til
 	attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-});
-
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
@@ -57,7 +52,6 @@ var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapn
 
 var baseMaps = {
     "Traditional": OpenMapSurfer_Roads,
-    "Topographical": OpenTopoMap,
     "Satellite": Esri_WorldImagery,
     "Greyscale": OpenStreetMap_BlackAndWhite
 }
@@ -65,7 +59,7 @@ var baseMaps = {
 var myMap = L.map("map", {
     center: [37, -100],
     zoom: 4,
-    layers: [OpenMapSurfer_Roads, foreclosureMarkers]
+    layers: [OpenStreetMap_BlackAndWhite, foreclosureMarkers]
 });
 
 var legend = L.control({ position: "bottomright" });
